bc29b02957927deb09b80a3c76157857fdc3a45f
KayTee1 <truong.kha@outlook.com>
2023-12-22 01:01:36 +0200

    catch all segments

---
+++ routing-demo/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type ParamsProps = {
+  params: {
+    slug: string[];
+  };
+};
+
+const Docs = ({ params }: ParamsProps) => {
+  if (params.slug?.length === 2) {
+    return (
+      <h1>
+        Viewing Docs for feature {params.slug[0]} and concept {params.slug[1]}
+      </h1>
+    );
+  } else if (params.slug?.length === 1) {
+    return <h1>Viewing Docs for feature {params.slug[0]}</h1>;
+  }
+  return <h1>Docs Home Page</h1>;
+};
+
+export default Docs;
